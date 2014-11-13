33ae92c97e80d5b3590193adebc3b524fcc05fe9
isaisaisa <louisa@spahl-hamburg.de>
2014-11-13 10:45:03 +0100

    changed a few things

--- Shop/app/models/produkt.rb
+++ Shop/app/models/produkt.rb
@@ -1,6 +1,8 @@
 class Produkt < ActiveRecord::Base
   attr_accessible :beschreibung, :bild, :gueltigBis, :gueltigVon, :name, :preis, :kategorie
   
+ 
+  
   def self.search(query)
     where("name LIKE (?)", "%#{query}%")
   end
