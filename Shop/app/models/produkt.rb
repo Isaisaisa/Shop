e8724cbe5e9ea0a11097d71d98c54458c27e1d2c
Eike-Christian Ramcke <webmaster@123-scripts.de>
2014-11-13 10:03:59 +0100

    Suche auch in Kategorie

--- Shop/app/models/produkt.rb
+++ Shop/app/models/produkt.rb
@@ -2,7 +2,7 @@ class Produkt < ActiveRecord::Base
   attr_accessible :beschreibung, :bild, :gueltigBis, :gueltigVon, :name, :preis, :kategorie
   
   def self.search(query)
-    where("name LIKE (?)", "%#{query}%")
+    where("name LIKE (?) OR kategorie LIKE (?)", "%#{query}%", "%#{query}%")
   end
   
 end
